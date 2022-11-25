fa20548f18e1eedefa4564abd0325f35e673033e
JayQY <105476611+JayQY@users.noreply.github.com>
2022-11-25 19:10:56 +0800

    Update Rewards.user.js

--- Rewards.user.js
+++ Rewards.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/JayQY/Rewards
 // @updateURL    https://raw.githubusercontent.com/JayQY/Rewards/main/Rewards.user.js
 // @downloadURL  https://raw.githubusercontent.com/JayQY/Rewards/main/Rewards.user.js
-// @version      1.0.21
+// @version      1.0.22
 // @description  Microsoft Rewards
 // @author       JayQY
 // @match        https://rewards.bing.com/
@@ -30,8 +30,8 @@ var _self = unsafeWindow, url = location.pathname, top = _self, parent = _self =
 
         runToday0();
 
-        for(var i = 0; i < 120; i++){
-            if(i < 95){
+        for(var i = 0; i < 60; i++){
+            if(i < 36){
                 setTimeout(() => ret0(wordlists[Math.floor(Math.random() * (2048 + 1))], autoRefresh), 5000 * i);
             }else{
                 setTimeout(() => ret1(wordlists[Math.floor(Math.random() * (2048 + 1))], autoRefresh), 5000 * i);
