fb5a9cda692aa563a8436b0e28261f7506897da2
JayQY <105476611+JayQY@users.noreply.github.com>
2023-07-07 11:36:46 +0800

    Create NewRewards.user.js

---
+++ NewRewards.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Rewards
+// @namespace    https://github.com/JayQY/Rewards
+// @updateURL    https://raw.githubusercontent.com/JayQY/Rewards/main/NewRewards.user.js
+// @downloadURL  https://raw.githubusercontent.com/JayQY/Rewards/main/NewRewards.user.js
+// @version      2.0.0
+// @description  Microsoft Rewards
+// @author       JayQY
+// @match        https://rewards.bing.com/
+// @match        *://www.bing.com/*
+// @match        *://www.msn.com/en-us/shopping
+// @match        *://www.msn.cn/en-us/shopping
+// @icon         https://www.google.com/s2/favicons?domain=bing.com
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @require      file://C:/Rewards.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant        window.focus
+// @grant        window.close
+// @connect      www.bing.com
+// @connect      rewards.bing.com
+// ==/UserScript==
